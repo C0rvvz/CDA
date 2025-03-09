cf5b9784198b0ff09076b249cbc0ce489918b4c4
C0rvvz <ajuandiego036@gmail.com>
2025-03-09 00:42:23 -0500

    Update pom.xml dependencies and clean up test configuration

--- src/test/java/udem/edu/co/CDA/CdaApplicationTests.java
+++ src/test/java/udem/edu/co/CDA/CdaApplicationTests.java
@@ -2,14 +2,12 @@ package udem.edu.co.cda;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class CdaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
