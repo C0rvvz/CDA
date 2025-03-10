d6b8f27c759b155c5e1393a1a78030f1c70d5986
C0rvvz <ajuandiego036@gmail.com>
2025-03-09 21:42:34 -0500

    Update EstudianteRepository to use Long as ID type and comment out contextLoads test

--- src/main/java/udem/edu/co/CDA/repository/EstudianteRepository.java
+++ src/main/java/udem/edu/co/CDA/repository/EstudianteRepository.java
@@ -2,8 +2,9 @@ package udem.edu.co.cda.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import udem.edu.co.cda.entities.Estudiante;
 
 @Repository
-public interface EstudianteRepository extends JpaRepository<Estudiante,String> {
+public interface EstudianteRepository extends JpaRepository<Estudiante,Long> {
 }
